37ed2b958119006fc1076436d72f5323add69381
Cesar Grossl <cgrossl00@gmail.com>
2024-03-29 13:45:14 -0300

    Inserido sql server

--- app/includes/classes/Db.php
+++ app/includes/classes/Db.php
@@ -7,6 +7,7 @@ class DB {
 	private $db;
 	private $user;
 	private $password;
+	private $consql;
 	private $consulta;
 	private $connportal;
 	//06051982 ABRE CONEXAO;
@@ -30,41 +31,74 @@ class DB {
 				$e->getMessage();
 				return false;
 			}
+		}elseif($conexao == 'totvs'){
+			$db_name = $db_name ? $db_name : DB_NAME_SQL;
+			$this->db = $db_name;
+			$db_host = $db_host ? $db_host : DB_HOST_SQL;
+			$this->host = $db_host;
+			$db_user = $db_user ? $db_user : DB_USER_SQL;
+			$this->user = $db_user;
+			$db_passwd = $db_passwd ? $db_passwd : DB_PASS_SQL;
+			$this->password = $db_passwd;
+            $dsn_sql = 'dblib:host='.$this->host.';dbname='.$this->db.';charset=LATIN-1';
+            try {
+    			$dbh_sql = new PDO($dsn_sql, $this->user, $this->password);
+				$dbh_sql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    			$this->consql = $dbh_sql;
+    		}
+    		catch ( PDOException $e ) {
+    			$e->getMessage();
+				return false;
+    		}
 		}
 	}
 	//06051982 FECHA CONEXAO;
 	public function FechaConexao($conexao='portal'){
 		if($conexao == 'portal'){
 			$this->connportal = null;
+		}elseif($conexao == 'totvs'){
+			$this->consql = null;
 		}
 	}
 	//06051982 SELECT FAZER POR AQUI;
 	public function select($query, $conexao="portal") {
-		if($conexao == 'portal'){
-			$this->consulta = $query;
-			try {
-				$query = $this->connportal->prepare($this->consulta);
-				$query->execute();
-				$retorna = $query->fetchAll(PDO::FETCH_ASSOC);
-				return $retorna;
-			}catch(PDOException $e) {
-				echo  $e->getMessage();
-				return false;
-			}
+		if($conexao == 'totvs'){
+			$this->query("SET ANSI_NULLS ON", $conexao);
+			$this->query("SET ANSI_WARNINGS ON", $conexao);
 		}
+		
+		$this->consulta = $query;
+		try {
+			if($conexao == 'totvs'){
+				$query = $this->consql->prepare($this->consulta);
+			}else{
+				$query = $this->connportal->prepare($this->consulta);
+			}
+			
+			$query->execute();
+			$retorna = $query->fetchAll(PDO::FETCH_ASSOC);
+			return $retorna;
+		}catch(PDOException $e) {
+			echo  $e->getMessage();
+			return false;
+		}
+		
 	}
 	//06051982 UPDATE INSERT, FAZER POR ESSE;
 	public function query($query, $conexao="portal") {
-		if($conexao == 'portal'){
-			$this->consulta = $query;
-			try {
+		$this->consulta = $query;
+		try {
+			if($conexao == 'totvs')
+				$query = $this->consql->prepare($this->consulta);
+			else{
 				$query = $this->connportal->prepare($this->consulta);
-				return $query->execute();
-			} catch (PDOException $e) {
-				echo  $e->getMessage();
-				return false;
 			}
+			return $query->execute();
+		} catch (PDOException $e) {
+			echo  $e->getMessage();
+			return false;
 		}
+		
 	}
 }
 ?>
--- app/includes/config.php
+++ app/includes/config.php
@@ -10,5 +10,11 @@
     define('DB_USER_PORTAL',$_ENV['MYSQL_USER']);
     define('DB_PASSWD_PORTAL',$_ENV['MYSQL_PASS']);
 
+    //Sql server Totvs
+    define('DB_HOST_SQL', "");
+    define('DB_USER_SQL', "");
+    define('DB_PASS_SQL', "");
+    define('DB_NAME_SQL', "");
+
 ?>
 
