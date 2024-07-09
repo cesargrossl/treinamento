00474ac6d454e05f7b59cb196f134cdaa1011863
Cesar Grossl <cgrossl00@gmail.com>
2024-07-08 21:52:38 -0300

    ajustes

--- app/cabecalho.php
+++ app/cabecalho.php
@@ -63,18 +63,12 @@
             <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
-                <h3>General</h3>
                 <ul class="nav side-menu">
-                  <li><a><i class="fa fa-home"></i> Home <span class="fa fa-chevron-down"></span></a>
-                    <ul class="nav child_menu">
-                      <li><a href="index.html">Dashboard</a></li>
-                      <li><a href="index2.html">Dashboard2</a></li>
-                      <li><a href="index3.html">Dashboard3</a></li>
-                    </ul>
+                  <li><a href="p_home.php"><i class="fa fa-home"></i> Início </a>
                   </li>
                   <li><a><i class="fa fa-cogs"></i> Configurações <span class="fa fa-chevron-down"></span></a>
                     <ul class="nav child_menu">
-                      <li><a href="form.html">Cad Usuários</a></li>
+                      <li><a href="p_cadusuario.php">Cadastro Usuários</a></li>
                     </ul>
                   </li>
                 </ul>
