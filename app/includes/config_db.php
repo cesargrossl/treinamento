965fb436161df94c327e5aed9d9bb2f0c50efa55
Cesar Grossl <cesar.grossl@buddemeyer.com.br>
2024-07-18 15:15:41 -0300

    ajuste

--- app/includes/config_db.php
+++ app/includes/config_db.php
@@ -12,7 +12,7 @@
     define('DB_HOST_SQL', "172.19.0.3");//IP PRECISA PEGAR DE DENTRO DO CONTAINER EM INSPECTOR, "IPAddress": "172.19.0.3",
     define('DB_USER_SQL', "sa");
     define('DB_PASS_SQL', "Cg5020@1223");
-    define('DB_NAME_SQL', "master");
+    define('DB_NAME_SQL', "db_curso");
 
 ?>
 
