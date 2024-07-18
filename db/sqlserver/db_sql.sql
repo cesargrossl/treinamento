e7c88caeea4f8931d8e199520f64bea50fca5258
Cesar Grossl <cesar.grossl@buddemeyer.com.br>
2024-07-18 15:01:40 -0300

    Ajustes

---
+++ db/sqlserver/db_sql.sql
@@ -0,0 +1,25 @@
+-- Criar o banco de dados db_curso, se ainda não existir
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'db_curso')
+BEGIN
+    CREATE DATABASE db_curso;
+    USE db_curso;
+END
+ELSE
+BEGIN
+    USE db_curso;
+END
+
+-- Criar a tabela TB_TESTE dentro do banco de dados db_curso
+IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'TB_TESTE')
+BEGIN
+    CREATE TABLE TB_TESTE (
+        ID INT PRIMARY KEY,
+        NOME VARCHAR(250)
+    );
+
+    -- Inserir dados na tabela TB_TESTE
+    INSERT INTO TB_TESTE (ID, NOME) VALUES (1, 'João');
+    INSERT INTO TB_TESTE (ID, NOME) VALUES (2, 'Maria');
+    INSERT INTO TB_TESTE (ID, NOME) VALUES (3, 'Carlos');
+    INSERT INTO TB_TESTE (ID, NOME) VALUES (4, 'Ana');
+END
